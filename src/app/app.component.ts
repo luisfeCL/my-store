78025c878d27959ea34d2e6a9e0aa83da3aba541
luisfeCL <92877836+luisfeCL@users.noreply.github.com>
2023-04-27 21:55:09 +0200

    [ADD] for and array manipulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './product.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,14 +9,51 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   name = 'Luisfe';
-  age = 30;
+  age = 18;
   img = 'https://www.w3schools.com/howto/img_avatar.png';
-  btnDisabled = true
+  btnDisabled: boolean = true
+  
   person = {
     name: 'Luisfe',
     age: 18,
     avatar: 'https://www.w3schools.com/howto/img_avatar.png'
   }
+
+  names: string[] = ['Luisfe', 'Nico', 'Santi'];
+  newName   = ''
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ];
 
   toggleButton(){
 
@@ -27,6 +65,15 @@ export class AppComponent {
 
     this.person.age += 1
 
+  }
+
+  addName(){
+    this.names.push(this.newName);
+    this.newName = ''
+  }
+
+  deleteName(index: number){
+    this.names.splice(index, 1)
   }
   
 }
